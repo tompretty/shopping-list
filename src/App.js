151ab6ec2eb0eeb2f9f23790aa3452b3c65dad78
TomPretty <tompretty2@gmail.com>
2020-05-23 18:55:35 +0100

    Add basic item form and item list
    Add simple form for adding new items as well as displaying
    the current list of items.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+function ItemsForm({ addItem }) {
+  const [quantity, setQuantity] = useState("");
+  const [name, setName] = useState("");
+  const quantityInput = useRef(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    addItem(quantity, name);
+    resetState();
+
+    quantityInput.current.focus();
+  }
+
+  function resetState() {
+    setQuantity("");
+    setName("");
+  }
+
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="number"
+        onChange={handleQuantityChange}
+        value={quantity}
+        ref={quantityInput}
+      />
+      <span>&times;</span>
+      <input type="text" onChange={handleNameChange} value={name} />
+      <button type="submit" hidden>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+function ItemsList({ items }) {
+  return (
+    <ul>
+      {items.map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.quantity}</span>
+            <span>&times;</span>
+            <span>{item.name}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(quantity, name) {
+    setItems([...items, { quantity: quantity, name: name }]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemsForm addItem={addItem} />
+      <ItemsList items={items} />
     </div>
   );
 }
